4b6fe7138b30416c9726bee44453b8390ab0a240
Lauren Holzman <36377466+thehellomartian@users.noreply.github.com>
2022-11-05 23:35:57 -0700

    Update sketch.js

--- libraries/sketch.js
+++ libraries/sketch.js
@@ -3,15 +3,21 @@ let img2;
 var value = 0;
 var dim = 600;
 var value2 = 0;
-var rad = 11;
+var rad = 10;
 let current;
 
-var places = [[208,37],[198,54],[240,106],[229,124],[316,128],[231,205],[249,328],[234,445]];
-var descriptions = ['A muddy field that is redeemed by the adjacent dog park.','The best place for cheap coffee!','The best street for dangerously fast sledding','Play a game of pickleball or search for the Geocache I have hidden here!', 'A summertime favorite for swimming and paddleboarding.','Excellent Bahn Mi and super friendly service!','They have possibly my favorite donuts to date and sell out quickly!','A fun park with excellent views at sunset.'];
+//var places = [[208,37],[198,54],[240,106],[229,124],[316,128],[231,205],[249,328],[234,445]];
+//var descriptions = ['A muddy field that is redeemed by the adjacent dog park.','The best place for cheap coffee.','The best part for dangerously fast sledding','Play a game of pickleball or search for the Geocache I have hidden here!', 'A summertime favorite for swimming and paddleboarding','Excellent Bahn Mi and super friendly service!','They have possibly my favorite donuts to date and sell out quickly!','A fun park with excellent views at sunset.'];
 
 function preload() {
-  img1 = loadImage('libraries/IMG_6112.PNG');
-  img2 = loadImage('libraries/seattleMap2.JPG');
+  img1 = loadImage('IMG_6112.PNG');
+  img2 = loadImage('IMG_7607.JPG');
+    img3 = loadImage('IMG_9696.JPG');
+
+    img4 = loadImage('numbat.JPG');
+
+
+  
   
 
 
@@ -57,11 +63,12 @@ function draw() {
   background(50)
   image(img2,dim/3,dim/2.5,119*3,159*3)
   
-  textSize(36);
+  textSize(12);
   fill(119,20,50);
- text('Press the left arrow to see a photo of my dog!',50,500,500,200)
- 
-  text('Click a red icon on the map to see more details below',2*dim/3,dim/10,dim/3)
+ text('(press the left arrow, I dare you. you will NOT be disappointed)',50,550,500,200)
+   textSize(42);
+
+  text('Happy Birthday to the best numbat of them all!',2*dim/3,dim/10,dim/3)
   if (value) {
   image(img1, mouseX, mouseY,75*resize,135*resize);
   }
